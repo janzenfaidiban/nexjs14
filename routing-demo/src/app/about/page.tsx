0e9ed2ca41813ecc8d0ad3ed3bd939469f8239ba
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-12-08 21:45:31 +0900

    routing metadata

--- routing-demo/src/app/about/page.tsx
+++ routing-demo/src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title: "About SaCode",
+}
+
 export default function About() {
     return <h1>About me</h1>
 }
--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,7 +1,24 @@
+import {Metadata} from "next";
+
+export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`)
+        }, 100)
+    });
+    return {
+        title: `Product ${params.productId}`
+    }
+}
+
+type Props = {
+    params: {
+        productId: string;
+    }
+}
+
 export default function ProductList({ 
     params ,
-}: {
-    params: { productId: string }
-}) {
+}: Props) {
     return <h1>Details about product {params.productId} </h1>
 }
