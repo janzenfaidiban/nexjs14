f832f825c0e959d236eba415688ae8774cd5d68e
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-12-08 22:04:46 +0900

    link component navigation

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,3 +1,8 @@
+import Link from 'next/link'
+
 export default function Home() {
-    return <h1>Welcome home...</h1>
+    return <><h1>Welcome home...</h1>
+        <Link href="/blog">Blog</Link> 
+        <Link href="/products">Products</Link>
+    </>
 }
