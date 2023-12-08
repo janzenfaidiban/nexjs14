56a8ca138fa0f802e9731ae367d8db44b6c8f6c8
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-12-08 21:54:59 +0900

    title Metadata

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "Blog",
+    },
+};
+
 export default function Blog() {
     return <h1>My blog...</h1>
 }
