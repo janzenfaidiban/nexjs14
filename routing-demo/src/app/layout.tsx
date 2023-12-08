bcec8ce21dd7e4d2a16f67048ca041954f75bb95
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-12-08 19:36:00 +0900

    layouts

--- routing-demo/src/app/layout.tsx
+++ routing-demo/src/app/layout.tsx
@@ -10,7 +10,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header
+          style={{
+            backgroundColor: "lightblue",
+            padding: "1rem",
+          }}
+        >
+          <p>Header</p>
+        </header>
+
+        {children}
+        
+        <footer
+          style={{
+            backgroundColor: "ghostwhite",
+            padding: "1rem",
+          }}>
+          <p>Footer</p>
+        </footer>
+      </body>
     </html>
   )
 }
